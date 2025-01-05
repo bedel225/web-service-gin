0d5606fceeb142dd8f887af699cb5031bf2da194
bedel225 <abdoulchoco@yahoo.fr>
2025-01-05 02:25:00 +0100

    factorisation du code

--- functions.go
+++ functions.go
@@ -9,3 +9,11 @@ import (
 func getAlbums(c *gin.Context) {
 	c.JSON(http.StatusOK, albums)
 }
+
+func getAlbumByID(c *gin.Context) {
+	for _, item := range albums {
+		if item.ID == c.Param("id") {
+			c.JSON(http.StatusOK, item)
+		}
+	}
+}
